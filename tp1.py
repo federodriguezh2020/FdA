d9a3b437ccc5e0f16820f0f285a0f8bb5c726456
naiveprogramming <63132486+naiveprogramming@users.noreply.github.com>
2020-04-17 22:04:28 -0300

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -1,44 +1,31 @@
 # TP1
 
+import sys
+
+nombre_comando = sys.argv[1]
+
 # Alumnos: Julieta De Antonio, Maria Gomez Alzaga y Federico Rodriguez
 
 # El numero primo que se encuentra el i posicion.
 # Para saber si un numero es primo, debo ver la
 # cantidad de divisores que tiene.
 # Funciones auxiliares:
-def numerosDivisores(n):
-    i = 1
-    divisores = []
-    while (i <= n):
-        if n % i == 0:
-            divisores += [i]
-        i= i + 1
-    return divisores
-    
-numerosDivisores(10)
+def esDivisor(i, n):
+    esDivisor = 0
+    if i <= n:
+        if i <= 0:
+            esDivisor = 1
+        else:    
+            if n % i != 0:
+                esDivisor = 1
+        if esDivisor == 0:
+            return True
+        else:
+            return False
 
-def divisores(n):
-    i = 1
-    divisores = []
-    while (i <= n):
-        if n % i == 0:
-            divisores += [i]
-        i= i + 1
-    if divisores != 0:
-        return True
-    
-divisores(7)
-
-def divisoresPrimos(n):
-    i = 2
-    divisoresPrimos = []
-    while i <= n:
-        if esPrimo(i) == True and # tengo que hacer una funcion esDivisor(i) == True:
-            divisoresPrimos += [i]
-        i += 1
-    return divisoresPrimos
-       
-divisoresPrimos(7)
+esDivisor(15,30)
+esDivisor(2,3)
+esDivisor(7,7)
 
 # Funciones
 def esPrimo(n):
@@ -52,51 +39,103 @@ def esPrimo(n):
                 esPrimo = 1
             i = i + 1
     if esPrimo == 0:
-        return True
+        return "sí"
     else:
-        return False
+        return "no"
 
-esPrimo(15)
+esPrimo(2)
+esPrimo(6)
 
-# Calculo auxiliar
+def divisoresPrimos(n):
+    i = 1
+    divisoresPrimos = []
+    while i <= n:
+        if esPrimo(i) == "sí": 
+            if esDivisor(i,n) == True:
+                divisoresPrimos += [i]
+        i += 1
+    return divisoresPrimos
+       
+divisoresPrimos(10)
+
+# Función auxiliar:
 def numerosPrimos(n):
     i = 2
     numerosPrimos = []
     while (i < n):
-        if esPrimo(i) == True :
+        if esPrimo(i) == "sí" :
             numerosPrimos += [i]
-        i= i + 1
+        i += 1
     return numerosPrimos
 
-numerosPrimos(5)
+numerosPrimos(13)
 
-# Funciones
+# Funciones:
 def iesimoPrimo(i):
-    n = 1000
-    return numerosPrimos(n)[i - 1]
-    
-iesimoPrimo(5)   
+    n = 1
+    while len(numerosPrimos(n)) < i + 1:
+        n += 1
+    return numerosPrimos(n)[i - 1]     
+
+iesimoPrimo(1)
+iesimoPrimo(5)
 
 def cantidadPrimosMenoresOIguales(n):
     return len(numerosPrimos(n + 1))
         
+cantidadPrimosMenoresOIguales(6)
 cantidadPrimosMenoresOIguales(7)
 
 def cantidadDivisoresPrimos(n):
     return len(divisoresPrimos(n))
  
 cantidadDivisoresPrimos(6)
+cantidadDivisoresPrimos(7)
 
-def sumaPrimerosPrimos(n): # REVISARRR
-    i = 2
+def iesimoDivisorPrimo(n, i):
+    if len(divisoresPrimos(n)) < i:
+        return (str(n) + " no tiene " + str(i) + " divisores primos")
+    else:
+        return divisoresPrimos(n)[i - 1]
+
+iesimoDivisorPrimo(10,1)
+iesimoDivisorPrimo(10,2)    
+iesimoDivisorPrimo(10,3)
+
+def sumaPrimerosPrimos(n):
+    i = 1
     suma = 0
     while i <= n:
-        suma += iesimoPrimo(i)
-        i =+ 1
+        suma += + iesimoPrimo(i)
+        i += 1
     return suma
 
+sumaPrimerosPrimos(3)
 sumaPrimerosPrimos(5)
-iesimoPrimo(5)
-    
-    
-    
+
+# Llamado a funciones
+if nombre_comando == "esPrimo":
+    n = int(sys.argv[2])
+    print(esPrimo(n))
+
+if nombre_comando == "iesimoPrimo":
+    i = int(sys.argv[2])
+    print(iesimoPrimo(i))
+
+
+if nombre_comando == "cantidadPrimosMenoresOIguales":
+    n = int(sys.argv[2])
+    print(cantidadPrimosMenoresOIguales(n))
+
+if nombre_comando == "cantidadDivisoresPrimos":
+    n = int(sys.argv[2])
+    print(cantidadDivisoresPrimos(n))
+
+if nombre_comando == "iesimoDivisorPrimo":
+    n = int(sys.argv[2])
+    i = int(sys.argv[3])
+    print(iesimoDivisorPrimo(n, i))
+
+if nombre_comando == "sumaPrimerosPrimos":
+    n = int(sys.argv[2])
+    print(sumaPrimerosPrimos(n))
