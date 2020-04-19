01b43978f428e8da74029b1766d08b3dd3c494df
mariagomezal <63408705+mariagomezal@users.noreply.github.com>
2020-04-19 14:21:15 -0300

    Update tp1.py

--- tp1.py
+++ tp1.py
@@ -1,15 +1,8 @@
 # TP1
 
-import sys
-
-nombre_comando = sys.argv[1]
-
 # Alumnos: Julieta De Antonio, Maria Gomez Alzaga y Federico Rodriguez
 
-# El numero primo que se encuentra el i posicion.
-# Para saber si un numero es primo, debo ver la
-# cantidad de divisores que tiene.
-# Funciones auxiliares:
+import sys 
 
 # Funciones:
 #1
@@ -27,8 +20,9 @@ def esPrimo(n):
         return "sí"
     else:
         return "no"
-#2    
-## Función auxiliar:
+
+# Funciones auxiliares:
+##
 def numerosPrimos(n):
     i = 2
     numerosPrimos = []
@@ -37,19 +31,8 @@ def numerosPrimos(n):
             numerosPrimos += [i]
         i += 1
     return numerosPrimos
-##
-def iesimoPrimo(i):
-    n = 1
-    while len(numerosPrimos(n)) < i + 1:
-        n += 1
-    return numerosPrimos(n)[i - 1]     
 
-#3  
-def cantidadPrimosMenoresOIguales(n):
-    return len(numerosPrimos(n + 1))
-        
-#4
-## Funciones auxiliares:    
+##
 def esDivisor(i, n):
     esDivisor = 0
     if i <= n:
@@ -62,7 +45,8 @@ def esDivisor(i, n):
             return True
         else:
             return False
-        
+
+##
 def divisoresPrimos(n):
     i = 1
     divisoresPrimos = []
@@ -72,48 +56,60 @@ def divisoresPrimos(n):
                 divisoresPrimos += [i]
         i += 1
     return divisoresPrimos
+       
 ##
+def iesimoNumeroPrimo(i):
+    n = 1
+    while len(numerosPrimos(n)) < i + 1:
+        n += 1
+    return numerosPrimos(n)[i - 1] 
+
+# Funciones:
+# 2
+def iesimoPrimo(i):
+    n = 1
+    while len(numerosPrimos(n)) < i + 1:
+        n += 1
+    return numerosPrimos(n)[i - 1]     
+
+# 3
+def cantidadPrimosMenoresOIguales(n):
+    return len(numerosPrimos(n + 1))
         
+# 4
 def cantidadDivisoresPrimos(n):
     return len(divisoresPrimos(n))
-#5
-
+ 
+# 5
 def iesimoDivisorPrimo(n, i):
     if len(divisoresPrimos(n)) < i:
         return (str(n) + " no tiene " + str(i) + " divisores primos")
     else:
         return divisoresPrimos(n)[i - 1]
-#6
+ 
+# 6
 def sumaPrimerosPrimos(n):
     i = 1
     suma = 0
     while i <= n:
-        suma += + iesimoPrimo(i)
+        suma += + iesimoNumeroPrimo(i)
         i += 1
     return suma
-  
-# Llamado a funciones
-if nombre_comando == "esPrimo":
-    n = int(sys.argv[2])
-    print(esPrimo(n))
 
-if nombre_comando == "iesimoPrimo":
-    i = int(sys.argv[2])
-    print(iesimoPrimo(i))
-
-if nombre_comando == "cantidadPrimosMenoresOIguales":
-    n = int(sys.argv[2])
-    print(cantidadPrimosMenoresOIguales(n))
-
-if nombre_comando == "cantidadDivisoresPrimos":
-    n = int(sys.argv[2])
-    print(cantidadDivisoresPrimos(n))
-
-if nombre_comando == "iesimoDivisorPrimo":
-    n = int(sys.argv[2])
-    i = int(sys.argv[3])
-    print(iesimoDivisorPrimo(n, i))
-
-if nombre_comando == "sumaPrimerosPrimos":
-    n = int(sys.argv[2])
-    print(sumaPrimerosPrimos(n))
+   
+# Llamado a funciones        
+funcion = sys.argv[1]
+parametro1 = int(sys.argv[2])
+if funcion == "esPrimo":
+    print(esPrimo(parametro1)) 
+elif funcion == "iesimoPrimo":
+    print(iesimoPrimo(parametro1))
+elif funcion == "cantidadPrimosMenoresOIguales":
+    print(cantidadPrimosMenoresOIguales(parametro1))
+elif funcion == "cantidadDivisoresPrimos":
+    print(cantidadDivisoresPrimos(parametro1))
+elif funcion == "sumaPrimerosPrimos":
+    print(sumaPrimerosPrimos(parametro1))
+elif funcion == "iesimoDivisorPrimo":
+    parametro2 = int(sys.argv[3])
+    print(iesimoDivisorPrimo(parametro1, parametro2))
