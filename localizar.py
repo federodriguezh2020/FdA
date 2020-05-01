8ec58be997c89c23f858bd868026f7fe1421e076
JulietaDA <63409773+JulietaDA@users.noreply.github.com>
2020-05-01 19:26:27 -0300

    Update localizar.py

--- localizar.py
+++ localizar.py
@@ -2,29 +2,28 @@ import sys
 
 from dependencia_judicial import DependenciaJudicial
 
-nombre_archivo = sys.argv[1]
-
 def cargar_dependencias(archivo):
-    dependencias = []
-    archivo = open(archivo)
+    dependencias = set()
+    archivo = open(archivo, encoding='latin-1')
     for linea in archivo:
-        atributos = linea.split(",")
-        fuero = atributos[1]
-        nombre = atributos[2]
-        tipo_de_ente = atributos[3]
-        direccion = atributos[4]
-        localidad = atributos[5]
-        departamento_judicial = atributos[6]
-        telefono = atributos[7]
-        latitud = atributos[8]
-        longitud = atributos[9]
-        dependencia = DependenciaJudicial(fuero, nombre, tipo_de_ente, direccion, localidad, departamento_judicial, telefono, latitud, longitud)
-        dependencias.append(dependencia)
+        atributos = linea.split(";")
+        numero = atributos[0]
+        if numero != "Número":
+            fuero = atributos[1]
+            nombre = atributos[2]
+            tipo_de_ente = atributos[3]
+            direccion = atributos[4]
+            localidad = atributos[5]
+            departamento_judicial = atributos[6]
+            telefono = atributos[7]
+            latitud = float(atributos[8])
+            longitud = float(atributos[9])
+            dependencia = DependenciaJudicial(numero, fuero, nombre, tipo_de_ente, direccion, localidad, departamento_judicial, telefono, latitud, longitud)
+            dependencias.add(dependencia)
     archivo.close()
     return dependencias
 
-dependencias = cargar_dependencias(nombre_archivo)
-
+dependencias = cargar_dependencias(sys.argv[1])
 
 latitud = sys.argv[2]
 longitud = sys.argv[3]
@@ -32,8 +31,8 @@ longitud = sys.argv[3]
 distancias = []
 
 for dependencia in dependencias:
-    if dependencia.latitud() != 0 and dependencia.longitud() !=0 :
-        distancias.add[dependencia.distancias(latitud, longitud)]
+    if dependencia.latitud() != 0 and dependencia.longitud() !=0:
+        distancias.append(dependencia.distancia(latitud, longitud))
+        distancias.sort()
 
-distancias.sort()
 print(distancias[0])
